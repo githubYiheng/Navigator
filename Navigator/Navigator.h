312b861cfdac62371c70a1074f36a18b71e8ec4d
 王传正 <chuanzheng.wang@asiainnovations.com>
2020-05-08 19:30:33 +0800

    first commit

---
+++ Navigator/Navigator.h
@@ -0,0 +1,19 @@
+//
+//  Navigator.h
+//  Navigator
+//
+//  Created by wang2gou on 2020/5/8.
+//  Copyright © 2020 Yiheng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Navigator.
+FOUNDATION_EXPORT double NavigatorVersionNumber;
+
+//! Project version string for Navigator.
+FOUNDATION_EXPORT const unsigned char NavigatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Navigator/PublicHeader.h>
+
+
